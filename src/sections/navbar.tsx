92c64b193a946f898f8e16070c853e03c2feab22
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-09-17 21:04:38 +0300

    create Navbar component to include responsive menu toggle

--- src/sections/navbar.tsx
+++ src/sections/navbar.tsx
@@ -1,4 +1,56 @@
-type Props = {};
-export function Navbar({}: Props) {
-  return <div>Navbar</div>;
+import { useState } from "react";
+import { navLinks } from "../constants";
+
+function NavItems() {
+  return (
+    <ul className="nav-ul">
+      {navLinks.map(({ id, href, name }) => (
+        <li key={id} className="nav-li">
+          <a href={href} className="nav-li_a" onClick={() => {}}>
+            {name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center py-5 mx-auto c-space">
+          <a
+            href="/"
+            className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+            Ričardas
+          </a>
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle menu">
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              alt="Toggle menu"
+              className="w-6 h-6"
+            />
+          </button>
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
+        </div>
+      </div>
+      <div className={`nav-sidebar ${isOpen ? "max-h-screen" : "max-h-0"}`}>
+        <nav className="p-5">
+          <NavItems />
+        </nav>
+      </div>
+    </header>
+  );
 }
