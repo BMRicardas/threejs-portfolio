cdedf613deb0f080a2a89d2a6e5e180b8d103c49
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-09-18 02:57:36 +0300

    fix component import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Hero } from "./sections/hero";
 import { About } from "./sections/about";
 import { Projects } from "./sections/projects";
 import { Clients } from "./sections/clients";
-import { WorkExperience } from "./sections/experience";
+import { WorkExperience } from "./sections/work-experience";
 import { Contact } from "./sections/contact";
 import { Footer } from "./sections/footer";
 
