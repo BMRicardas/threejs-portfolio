a05fff493ae57f0bc582431b278b5ddd933df9f6
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-09-17 20:31:09 +0300

    refactor App component to include Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Navbar } from "./sections/navbar";
+
 export function App() {
   return (
     <main className="max-w-7xl">
-      <h1 className="text-2xl text-white underline">Hello world</h1>
+      <Navbar />
     </main>
   );
 }
